2782fcb01c9a616e3622a2dcea9b689853ab63c2
Syed_Mubashir_Alam <mubashiralam02@gmail.com>
2023-04-12 10:51:50 +0500

    Test 2 Passed!

--- src/main/java/com/example/Cart.java
+++ src/main/java/com/example/Cart.java
@@ -5,8 +5,19 @@ import java.util.List;
 
 public class Cart {
     List<Item> items = new ArrayList<>();
+    int totalPrice = 0;
 
     public int getCart(){
         return items.size();
+    }
+
+    public void addItem(Item item){
+        int total = item.price * item.quantity;
+        totalPrice += total;
+        items.add(item);
+    }
+
+    public int getTotalPrice(){
+        return totalPrice;
     }
 }
